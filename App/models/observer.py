a6879a6f2ad4ae03a6c18363ef3fe3cf3d24d0ca
kieron <kieronspersad@gmail.com>
2025-11-20 19:25:40 -0400

    feat(observer): update() method now uses subject: Any and message: str

--- App/models/observer.py
+++ App/models/observer.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 class Observer(ABC):
     @abstractmethod
-    def update(self, subject: "Subject") -> None:
+    def update(self, subject: Any, message: str = None) -> None:
         pass
