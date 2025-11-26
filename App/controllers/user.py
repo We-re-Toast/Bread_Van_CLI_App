a3bf02aac34a5d266e80dff7592e15504f4dec48
AaronMaharaj <aaronmaharaj00@gmail.com>
2025-11-26 18:09:48 -0400

    Refactored initialize and user controllers

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,21 +1,26 @@
 from App.models import User, Driver
 from App.database import db
 
+
 def create_user(username, password):
     newuser = User(username=username, password=password)
     db.session.add(newuser)
     db.session.commit()
     return newuser
 
+
 def get_user_by_username(username):
     result = db.session.execute(db.select(User).filter_by(username=username))
     return result.scalar_one_or_none()
 
+
 def get_user(id):
     return db.session.get(User, id)
 
+
 def get_all_users():
     return db.session.scalars(db.select(User)).all()
+
 
 def get_all_users_json():
     users = get_all_users()
@@ -23,6 +28,7 @@ def get_all_users_json():
         return []
     users = [user.get_json() for user in users]
     return users
+
 
 def update_user(id, username):
     user = get_user(id)
@@ -32,23 +38,3 @@ def update_user(id, username):
         db.session.commit()
         return True
     return None
-
-def user_login(username, password):
-    user = db.session.execute(db.select(User).filter_by(username=username)).scalar_one_or_none()
-    if user and user.check_password(password):
-        user.logged_in = True
-        if isinstance(user, Driver):
-            user.status = "Available"
-        db.session.commit()
-        return user
-    raise ValueError("Invalid username or password.")
-
-def user_logout(user):
-    user.logged_in = False
-    if isinstance(user, Driver):
-        user.status = "Offline"
-    db.session.commit()
-    return user
-
-def user_view_street_drives(user, area_id, street_id):
-    return user.view_street_drives(area_id, street_id)
