0ecd598ce988f3bb5d84d33badecda58fd9560e5
VikashBi <157636289+VikashBi@users.noreply.github.com>
2025-11-26 13:58:40 -0400

    deleted driver stock file, refactored driver, item, resident and renamed stop to stop request

---
+++ App/models/stop_request.py
@@ -0,0 +1,23 @@
+from App.database import db
+
+
+class StopRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String(200), nullable=True)
+
+    """Foreign keys"""
+    resident_id = db.Column(db.Integer, db.ForeignKey("resident.id"), nullable=False)
+    drive_id = db.Column(db.Integer, db.ForeignKey("drive.id"), nullable=False)
+
+    def __init__(self, drive_id, resident_id, message):
+        self.drive_id = drive_id
+        self.resident_id = resident_id
+        self.message = message
+
+    def get_json(self):
+        return {
+            "id": self.id,
+            "drive_id": self.drive_id,
+            "resident_id": self.resident_id,
+            "message": self.message,
+        }
